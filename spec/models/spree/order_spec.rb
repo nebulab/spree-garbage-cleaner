475e0f5e627b13d42300546f01d77c3c278d3eaa
Matteo Latini <mtylty@gmail.com>
2012-08-07 17:41:16 +0200

    adds some tests for order

--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -1,12 +1,36 @@
 require 'spec_helper'
 
 describe Spree::Order do
-  context "when order is incomplete" do
-    before do
-       # # Field required to complete the order 
-       # order.bill_address = Factory(:address)
-       # order.ship_address = Factory(:address)
-       # Factory(:inventory_unit, :order => order, :state => 'shipped')
-    end
+  it "has a garbage finder method" do
+    ordered_on = Spree::GarbageCleaner::Config.cleanup_days_interval
+
+    order_one = Factory(:order, :created_at => (ordered_on+rand(10)).days.ago, :completed_at => nil)
+    order_two = Factory(:order, :created_at => (ordered_on+rand(10)).days.ago, :completed_at => nil)
+    order_three = Factory(:order)
+
+    Spree::Order.garbage.should == [order_one, order_two]
+  end
+
+  it "has a method that tells if order is garbage" do
+    order = Factory.build(:order)
+    order.should respond_to(:garbage?)
+  end
+
+  it "is garbage if not completed and past cleanup_days_interval" do
+    ordered_on = Spree::GarbageCleaner::Config.cleanup_days_interval
+    order = Factory.build(:order, :created_at => ordered_on.days.ago, :completed_at => nil)
+    order.garbage?.should be_true
+  end
+
+  it "is not garbage if not completed and not past cleanup_days_interval" do
+    ordered_on = Spree::GarbageCleaner::Config.cleanup_days_interval
+    order = Factory.build(:order, :created_at => (ordered_on-1).days.ago, :completed_at => nil)
+    order.garbage?.should be_false
+  end
+
+  it "is not garbage if completed and past cleanup_days_interval" do
+    ordered_on = Spree::GarbageCleaner::Config.cleanup_days_interval
+    order = Factory.build(:order, :created_at => ordered_on.days.ago, :completed_at => Time.now)
+    order.garbage?.should be_false
   end
 end
