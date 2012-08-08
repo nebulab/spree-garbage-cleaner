a82102704a5bd254ae97d2a6bde37e7309d7adf6
Matteo Latini <mtylty@gmail.com>
2012-08-08 17:22:59 +0200

    adds proper garbage methods for user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -6,12 +6,16 @@ module Spree
     
     def self.garbage
       garbage_after = Spree::GarbageCleaner::Config.cleanup_days_interval
-      self.where("(email IS NULL OR email LIKE ?) AND created_at <= ?", '%@example.net', garbage_after.days.ago)
+      self.joins('LEFT JOIN spree_orders ON spree_orders.user_id = spree_users.id').where("
+          (spree_users.email IS NULL OR spree_users.email LIKE ?) AND 
+          spree_users.created_at <= ? AND spree_orders.completed_at IS NULL
+        ", '%@example.net', garbage_after.days.ago
+      )
     end
 
     def garbage?
       garbage_after = Spree::GarbageCleaner::Config.cleanup_days_interval
-      anonymous? && created_at <= garbage_after.days.ago
+      anonymous? && created_at <= garbage_after.days.ago && orders.count == orders.incomplete.count
     end
   end
 end
--- spec/models/spree/user_spec.rb
+++ spec/models/spree/user_spec.rb
@@ -5,12 +5,18 @@ describe Spree::User do
 
   context "class methods" do
     before do
-      @user_one = Spree::User.anonymous!
-      @user_two = Spree::User.anonymous!
-      @user_three = Factory(:user)
+      @user_one = Spree::User.anonymous!   # garbage
+      @user_two = Spree::User.anonymous!   # garbage
+      @user_three = Spree::User.anonymous! # not garbage
+      @user_four = Factory(:user)          # not garbage
+
+      Factory(:order, :user => @user_one)
+      Factory(:order, :user => @user_two)
+      Factory(:completed_order_with_totals, :user => @user_three)
 
       @user_one.update_column(:created_at, (created_on+rand(10)).days.ago)
       @user_two.update_column(:created_at, (created_on+rand(10)).days.ago)
+      @user_three.update_column(:created_at, (created_on+rand(10)).days.ago)
     end
 
     it "has a garbage finder method" do
@@ -20,7 +26,7 @@ describe Spree::User do
     it "has a method to destroy garbage" do
       Spree::User.destroy_garbage.should == [@user_one, @user_two]
       Spree::User.garbage.count.should == 0
-      Spree::User.all.should == [@user_three]
+      Spree::User.all.should include(@user_three, @user_four)
     end
   end
 
@@ -49,6 +55,13 @@ describe Spree::User do
 
     it "is not garbage if not anonymous and not past cleanup_days_interval" do
       user = Factory.build(:user)
+      user.garbage?.should be_false
+    end
+
+    it "is not garbage if it has a completed order associated to" do
+      user = Spree::User.anonymous!
+      user.update_column(:created_at, created_on.days.ago)
+      order = Factory(:completed_order_with_totals, :user => user)
       user.garbage?.should be_false
     end
   end
