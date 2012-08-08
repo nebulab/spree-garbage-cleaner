49adbd32cc8a9b4d38b357668f0b9fcd140fe378
Matteo Latini <mtylty@gmail.com>
2012-08-08 17:34:53 +0200

    refactors to add some readability

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -6,11 +6,10 @@ module Spree
     
     def self.garbage
       garbage_after = Spree::GarbageCleaner::Config.cleanup_days_interval
-      self.joins('LEFT JOIN spree_orders ON spree_orders.user_id = spree_users.id').where("
-          (spree_users.email IS NULL OR spree_users.email LIKE ?) AND 
-          spree_users.created_at <= ? AND spree_orders.completed_at IS NULL
-        ", '%@example.net', garbage_after.days.ago
-      )
+      garbage = self.joins('LEFT JOIN spree_orders ON spree_orders.user_id = spree_users.id')
+      garbage = garbage.where("spree_users.email IS NULL OR spree_users.email LIKE ?", '%@example.net')
+      garbage = garbage.where("spree_users.created_at <= ?", garbage_after.days.ago)
+      garbage.where("spree_orders.completed_at IS NULL")
     end
 
     def garbage?
